892fb903051dee325640c6704e25ddfbdf1cf5dd
Alazar351 <121960308+Alazar351@users.noreply.github.com>
2023-07-22 12:56:56 -0500

    Update background.js
    removed console.log

--- background.js
+++ background.js
@@ -2,13 +2,9 @@ chrome.runtime.onMessage.addListener((data) => {
   const { event, prefs } = data;
   switch (event) {
     case "start":
-      console.log("start works");
-      console.log(prefs);
-      console.log(prefs.currentlyActive);
       handleOnStart(prefs);
       break;
     case "pause":
-      console.log("pause works");
       handleOnPause(prefs);
       break;
     default:
@@ -18,7 +14,7 @@ chrome.runtime.onMessage.addListener((data) => {
 
 const handleOnStart = (prefs) => {
   chrome.storage.local.set(prefs, function () {
-    console.log("stored data");
+
   });
 
   prefrences = prefs;
@@ -28,11 +24,6 @@ const handleOnStart = (prefs) => {
       {
         displayMessage:
           "Something went wrong! Please press reset to begin again",
-      },
-      () => {
-        console.log(
-          `interval is greater than frequency  freq: ${selectedFreq} interval: ${intervalCount}`
-        );
       }
     );
   } else if (storedHours && storedAmount) {
@@ -40,44 +31,33 @@ const handleOnStart = (prefs) => {
       const { alarmCounter } = result;
       if (alarmCounter) {
         intervalCount = alarmCounter;
-        console.log("interval count = " + intervalCount);
       }
     });
     chrome.alarms.create("timer", {
       delayInMinutes: prefrences.alarmTimer,
       periodInMinutes: prefrences.alarmTimer,
     });
-    console.log(`alarm created. delayInMinutes = ${prefrences.alarmTimer}`);
   }
 };
 
 chrome.alarms.onAlarm.addListener(() => {
-  console.log("alarm started");
   const { selectedFreq } = prefrences;
 
   createNotifications(prefrences);
-  console.log(
-    "intervalCount and selectedFrequency " + intervalCount + " " + selectedFreq
-  );
-
+ 
   if (intervalCount == selectedFreq) {
     chrome.storage.local.set(
       {
         displayMessage:
           "Congratulations! You are hydrated! Press reset to begin again",
-      },
-      () => {
-        console.log("interval is equal to frequency");
       }
     );
     chrome.alarms.clearAll();
-    console.log("cleared all alarms");
+
     intervalCount++;
   }
 
-  chrome.storage.local.set({ alarmCounter: intervalCount }, () => {
-    console.log("interval has been set to " + intervalCount);
-  });
+  chrome.storage.local.set({ alarmCounter: intervalCount });
 });
 let prefrences;
 let intervalCount = 1;
@@ -94,10 +74,6 @@ const createNotifications = (prefs) => {
 };
 
 const handleOnPause = (prefs) => {
-  chrome.storage.local.set(prefs, function () {
-    console.log("pause stored data");
-    console.log(prefs);
-  });
+  chrome.storage.local.set(prefs);
   chrome.alarms.clearAll();
-  console.log("alarms cleared");
 };
